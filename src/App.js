725afb65f83f6d9929b78e411fc751618559cca6
Avijit Pateriya <deviljit.ap@gmail.com>
2023-05-05 23:44:29 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Carousel from "./components/Carousel/Carousel";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Portfolio from "./components/Portfolio/Portfolio";
-
+import ChatIcon from "@mui/icons-material/Chat";
 function App() {
   return (
     <div className="App">
@@ -13,6 +13,10 @@ function App() {
       <About />
       <Portfolio />
       <Footer />
+      <div className="sticky-div">
+        <ChatIcon style={{ color: "white" }} />
+        <button>Let's Chat</button>
+      </div>
     </div>
   );
 }
